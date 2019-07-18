cc39f9452a7eed4755adb35604fdf8be49d7bd03
ujjwal kumar <pathakujjwal93@gmail.com>
2019-07-18 18:12:00 +0530

    templates added

--- djangogirl/urls.py
+++ djangogirl/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    #path('', views.post_list, name='post_list'),
 ]
